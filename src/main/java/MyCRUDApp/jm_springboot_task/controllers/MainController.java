5e1f85aa451760f77766ba11e1ce5f93d1ef374b
Babaiqa <ru_com@mail.ru>
2021-03-26 12:46:38 +0500

    Java pre-project. Практическая задача 3.1.3 решена! *удален лишний контроллер(MainController + UserController);

--- src/main/java/MyCRUDApp/jm_springboot_task/controllers/MainController.java
+++ src/main/java/MyCRUDApp/jm_springboot_task/controllers/MainController.java
@@ -1,16 +1,40 @@
 package MyCRUDApp.jm_springboot_task.controllers;
 
+import MyCRUDApp.jm_springboot_task.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping("/admin")
+@RequestMapping()
 public class MainController {
 
-    @GetMapping("")
+    private final UserService userService;
+
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/admin")
     public String homePage() {
 
         return "users";
+    }
+    @GetMapping("/user")
+    public String showUserData(Principal principal, Model model) {
+        model.addAttribute("user", userService.findByEmail(principal.getName()));
+        return "showUser";
+    }
+
+
+    @GetMapping(value = "/login")
+    public String getLoginPage(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "login";
     }
 }
